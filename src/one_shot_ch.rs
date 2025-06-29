d4874366e5246734ee74653ae7e940ed29858b1e
Li Feng <maplestarplay@gmail.com>
2025-06-29 10:23:04 +0800

    feat: ensure safety through type systm

--- src/one_shot_ch.rs
+++ src/one_shot_ch.rs
@@ -1,47 +1,54 @@
 use std::cell::UnsafeCell;
 use std::mem::MaybeUninit;
+use std::sync::Arc;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering;
 
 pub struct Channel<T> {
     message: UnsafeCell<MaybeUninit<T>>,
     ready: AtomicBool,
-    in_use: AtomicBool,
+}
+
+pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+    let ch = Arc::new(Channel {
+        message: UnsafeCell::new(MaybeUninit::uninit()),
+        ready: AtomicBool::new(false),
+    });
+    (
+        Sender {
+            channel: ch.clone(),
+        },
+        Receiver { channel: ch },
+    )
 }
 
 unsafe impl<T> Sync for Channel<T> where T: Send {}
-unsafe impl<T> Send for Channel<T> where T: Send {}
+pub struct Sender<T> {
+    channel: Arc<Channel<T>>,
+}
 
-impl<T> Channel<T> {
-    pub fn new() -> Self {
-        Self {
-            message: UnsafeCell::new(MaybeUninit::uninit()),
-            ready: AtomicBool::new(false),
-            in_use: AtomicBool::new(false),
+pub struct Receiver<T> {
+    channel: Arc<Channel<T>>,
+}
+
+impl<T> Sender<T> {
+    pub fn send(self, message: T) {
+        unsafe {
+            (*self.channel.message.get()).write(message);
         }
+        self.channel.ready.store(true, Ordering::Release);
     }
+}
 
-    pub unsafe fn send(&self, message: T) {
-        if !self.in_use.swap(true, Ordering::Acquire) {
-            panic!("can't send more than once")
-        }
-        unsafe { (*self.message.get()).write(message) };
-
-        self.ready.store(true, Ordering::Release);
-    }
-
+impl<T> Receiver<T> {
     pub fn is_ready(&self) -> bool {
-        self.ready.load(Ordering::Acquire)
+        self.channel.ready.load(Ordering::Relaxed)
     }
-
-    pub unsafe fn receive(&self) -> T {
-        if !self.is_ready() {
-            panic!("no message available");
+    pub fn receive(self) -> T {
+        if !self.channel.ready.swap(false, Ordering::Acquire) {
+            panic!("no available message");
         }
-        if !self.ready.swap(false, Ordering::Acquire) {
-            panic!("no message available")
-        }
-        unsafe { (*self.message.get()).assume_init_read() }
+        unsafe { (*self.channel.message.get()).assume_init_read() }
     }
 }
 
@@ -56,24 +63,21 @@ impl<T> Drop for Channel<T> {
 #[cfg(test)]
 mod tests {
     use std::thread;
-
     use super::*;
 
-    // #[test]
-    // fn one_shot_channel_basic() {
-    //     let ch = Channel::new();
-    //     let t = thread::current();
-    //     thread::scope(|s| {
-    //         s.spawn(|| {
-    //             unsafe { ch.send("a") };
-    //             t.unpark();
-    //         });
-            
-    //         while !ch.is_ready() {
-    //             thread::park();
-    //         }
-
-    //         assert_eq!(unsafe { ch.receive() }, "a")
-    //     });
-    // }
+    #[test]
+    fn main() {
+        thread::scope(|s| {
+            let (sender, receiver) = channel();
+            let t = thread::current();
+            s.spawn(move || {
+                sender.send("a");
+                t.unpark();
+            });
+            while !receiver.is_ready() {
+                thread::park();
+            }
+            assert_eq!(receiver.receive(), "a");
+        });
+    }
 }
